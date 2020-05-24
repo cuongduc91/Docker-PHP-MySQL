67acab605df87b1fe1dc8ab75b5ea95eb36b1090
Cuong <bdcuong91@gmail.com>
2020-05-24 20:26:34 +0200

    Initializing with Docker, PHP and MySQL

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP-Section</title>
+</head>
+<body>
+<?php
+  $title = "This is my site"
+?>
+  <h1> <?php echo $title ?></h1>
+</body>
+</html>
