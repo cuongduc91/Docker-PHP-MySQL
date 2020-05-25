a25823e9a729e823f56fd66c5966f556b007a65f
Cuong <bdcuong91@gmail.com>
2020-05-25 10:01:49 +0200

    mysqli connect localhost, dbeaver

--- src/index.php
+++ src/index.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP-Section</title>
 </head>
+
 <body>
-<?php
-  $title = "This is my site"
-?>
-  <h1> <?php echo $title ?></h1>
+  <?php
+
+  $mysqli = new mysqli('db', getenv('MYSQL_USER'), getenv('MYSQL_ROOT_PASSWORD'), 'information_schema');
+
+  if ($mysqli->connect_error) {
+    echo 'Connection Error [', $mysqli->connect_errno, ']: ', $mysqli->connect_error;
+  } else {
+    echo 'MySQLi Connected Successfully!';
+  }
+
+  ?>
 </body>
+
 </html>
